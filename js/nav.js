08e6483dd7941f8bd841583ab94eb220a1f0e11b
mocxd <mark.adkins.hastings@gmail.com>
2015-08-07 20:12:01 -0400

    starts js, improves styles'

---
+++ js/nav.js
@@ -0,0 +1,17 @@
+// Slides the side navbar in and out like a boss
+function toggleNav () {
+
+    // grab the navBtn element
+    var nav = document.getElementById('sideNav');
+
+    // change the width depending on whether it is expanded
+    if (nav.style.width === '0vw') {
+        nav.style.width = '70vw';
+        // set the transitions independently to achieve a 'flipped' easing
+        nav.style.transition = 'width 500ms cubic-bezier(0.165, 0.840, 0.440, 1.000)';
+    } else {
+        nav.style.width = '0vw';
+        nav.style.transition = 'width 500ms cubic-bezier(0.895, 0.030, 0.685, 0.220)';
+    }
+
+}
---
+++ js/validations.js
@@ -0,0 +1,10 @@
+// Does validtions of the form fields, like a boss
+function validate () {
+    console.log('validatin');
+
+    var user = document.getElementById('username');
+    var pass = document.getElementById('password');
+
+}
+
+// Toggles password visibility, like a miniboss
